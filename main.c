6bdbe269007ee020e62b2cba9877f794443f6a99
Eiden <eidnegabriel07@gmail.com>
2022-05-12 18:44:55 -0400

    monty header

---
+++ main.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+int main (int argc, char *argv[])
+{
+   FILE *fo;
+   char *line = NULL;
+   size_t len = 0;
+   ssize_t read;
+   char *first;
+   char *second;
+
+   if (argc != 2)
+   {
+        fprintf(stderr,"USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+   }
+
+   fo = fopen(argv[1], "r");
+   if (fo == NULL)
+   {
+        fprintf(stderr,"Error: Can't open file\n");
+        exit(EXIT_FAILURE);
+   }
+
+   while ((read = getline(&line, &len, fo)) != -1) 
+   {
+       first = strtok(line, " \t\n");
+       second = strtok(NULL, " \t\n");
+       printf("%s %s\n", first, second);
+   }
+   
+   free(line);
+   exit(EXIT_SUCCESS);
+}
