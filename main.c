27bfec0e4964334c0738f287c99b50a0b043a4ad
Eiden <eidnegabriel07@gmail.com>
2022-05-12 18:47:43 -0400

    betty

--- main.c
+++ main.c
@@ -1,34 +1,40 @@
 #include "monty.h"
+/**
+ * main - check the code
+ *@argc: argument count
+ *@argv: argument value
+ * Return: Always 0.
+ */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-   FILE *fo;
-   char *line = NULL;
-   size_t len = 0;
-   ssize_t read;
-   char *first;
-   char *second;
+	FILE *fo;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char *first;
+	char *second;
 
-   if (argc != 2)
-   {
-        fprintf(stderr,"USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-   }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-   fo = fopen(argv[1], "r");
-   if (fo == NULL)
-   {
-        fprintf(stderr,"Error: Can't open file\n");
-        exit(EXIT_FAILURE);
-   }
+	fo = fopen(argv[1], "r");
+	if (fo == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file\n");
+		exit(EXIT_FAILURE);
+	}
 
-   while ((read = getline(&line, &len, fo)) != -1) 
-   {
-       first = strtok(line, " \t\n");
-       second = strtok(NULL, " \t\n");
-       printf("%s %s\n", first, second);
-   }
-   
-   free(line);
-   exit(EXIT_SUCCESS);
+	while ((read = getline(&line, &len, fo)) != -1)
+	{
+		first = strtok(line, " \t\n");
+		second = strtok(NULL, " \t\n");
+		printf("%s %s\n", first, second);
+	}
+
+	free(line);
+	exit(EXIT_SUCCESS);
 }
